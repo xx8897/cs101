094ecc8001df51bf5f3828c495826f4e1d99e186
ZhiXiang <55120640+xx8897@users.noreply.github.com>
2022-12-21 07:56:33 +0000

    main3.c

--- hw6/main3.c
+++ hw6/main3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-    int year=1900;
+    int year=2022;
     if(year%400==0)printf("閏年");
     else if ((year%100!=0)&&(year%4==0))printf("閏年");
     else printf("不是閏年");
