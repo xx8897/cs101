96baef95a256795d2dee177e58f42e70c53755ad
ZhiXiang <55120640+xx8897@users.noreply.github.com>
2022-12-29 08:52:01 +0000

    qz2

--- qz2/main.c
+++ qz2/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void print_spaces(int r){
-    int rows=5;
-    for(int i=rows-r;i>0;i--){
+void print_spaces(int r,int rows){
+    int i;
+    for(i=0;rows-r>i;i++){
         printf("  ");
     }
 }
 
 void print_stars(int r){
-    int rows=5;
+    int i;
     for(int i=0;i<2*r-1;i++){
         printf("* ");
     }
@@ -17,7 +17,7 @@ void print_stars(int r){
 int main(){
     int rows=5;
     for(int i=rows;i>=1;--i){
-        print_spaces(i);
+        print_spaces(i,rows);
         print_stars(i);
         printf("\n");
     }
